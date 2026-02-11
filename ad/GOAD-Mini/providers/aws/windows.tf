6abcdc169a09713144e05fd6ce694aef6b9599bc
Ioannis Polymenis <giannis.polymenis@gmail.com>
2026-02-11 09:20:35 +0000

    update AWS AMIs for Windows instances across multiple configurations

--- ad/GOAD-Mini/providers/aws/windows.tf
+++ ad/GOAD-Mini/providers/aws/windows.tf
@@ -2,7 +2,7 @@
   name               = "dc01"
   domain             = "sevenkingdoms.local"
   windows_sku        = "2019-Datacenter"
-  ami                = "ami-03440f0d88fea1060"
+  ami                = "ami-0567a6cf90bca0518"
   instance_type      = "t2.medium"
   private_ip_address = "{{ip_range}}.10"
   password           = "8dCT-DJjgScp"
